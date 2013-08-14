d730660e1e10ff08936e759abc72f742199a109d
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-08-14 16:36:22 +0200

    useless imports

--- deployment-repository-api/src/main/java/com/peergreen/deployment/repository/search/RepositoryQuery.java
+++ deployment-repository-api/src/main/java/com/peergreen/deployment/repository/search/RepositoryQuery.java
@@ -1,12 +1,5 @@
 package com.peergreen.deployment.repository.search;
 
-import com.peergreen.deployment.repository.filter.Filter;
-import com.peergreen.deployment.repository.filter.StringFilter;
-import com.peergreen.deployment.repository.search.AttributeQuery;
-import com.peergreen.deployment.repository.search.Occur;
-import com.peergreen.deployment.repository.search.Query;
-import com.peergreen.deployment.repository.search.QueryVisitor;
-
 /**
  * @author Mohammed Boukada
  */
